8601bbbf5f811e9333425839f84eecace6ea19b7
ArneshMaurya <maurya.arnesh07@gmail.com>
2020-05-14 10:59:44 +0530

    Level 2 added

--- Assets/Scenes/RockectBoost.cs
+++ Assets/Scenes/RockectBoost.cs
@@ -4,33 +4,76 @@ using UnityEngine;
 
 public class RockectBoost : MonoBehaviour {
     Rigidbody rigidBody;
-    Vector3 m_EulerAngleVelocity;
     AudioSource m_MyAudioSource;
 
     //Play the music
     bool m_Play;
-    //Detect when you use the toggle, ensures music isn’t played multiple times
     bool m_ToggleChange;
     [SerializeField] float rcsTrust = 100f;
     [SerializeField] float mainTrust = 80f;
 
-    // Start is called before the first frame update
     void Start () {
         rigidBody = GetComponent<Rigidbody> ();
-        //m_EulerAngleVelocity = new Vector3(0, 100, 0);
-
-        //Fetch the AudioSource from the GameObject
         m_MyAudioSource = GetComponent<AudioSource> ();
-        //Ensure the toggle is set to true for the music to play at start-up
         m_Play = true;
     }
 
     // Update is called once per frame
     void Update () {
-        // Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
-        //rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
         Thrust ();
         Rotate ();
+        //TouchControll ();
+    }
+
+    private void TouchControll () {
+
+        print ("IN side TouchControll");
+
+        if ((Input.touchCount > 0) && (Input.GetTouch (0).phase == TouchPhase.Began)) {
+            Ray raycast = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+            RaycastHit raycastHit;
+            print ("IN side 1ft if");
+            if (Physics.Raycast (raycast, out raycastHit)) {
+                Debug.Log ("Something Hit");
+                if (raycastHit.collider.name == "Soccer") {
+                    Debug.Log ("Soccer Ball clicked");
+                }
+
+                //OR with Tag
+
+                if (raycastHit.collider.CompareTag ("UpKey")) {
+                    Debug.Log ("Upkey clicked");
+                }
+
+                if (raycastHit.collider.CompareTag ("RightKey")) {
+                    Debug.Log ("Rightkey clicked");
+                }
+
+                if (raycastHit.collider.CompareTag ("LeftKey")) {
+                    Debug.Log ("Leftkey clicked");
+                }
+
+            }
+        }
+    }
+
+    void OnCollisionEnter (Collision collision) {
+
+        foreach (ContactPoint contact in collision.contacts) {
+            Debug.DrawRay (contact.point, contact.normal, Color.white);
+        }
+
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                print ("alive");
+                break;
+            case "Fuel":
+                //
+                break;
+            default:
+                print ("Dead !!");
+                break;
+        }
 
     }
 
